91bc07ae7485d742a0c159098f7655b6984e1e08
boost-ogawa <tak.ogawa.vc@gmail.com>
2025-04-29 13:29:54 +0900

    Update app.py

--- app.py
+++ app.py
@@ -150,7 +150,7 @@ def get_user_data(github_raw_url, nickname, user_id):
 if "row_to_load" not in st.session_state:
     st.session_state.row_to_load = 0
 if "fixed_row_index" not in st.session_state:
-    st.session_state.fixed_row_index = 1
+    st.session_state.fixed_row_index = 2
 if "page" not in st.session_state:
     st.session_state.page = 0
 if "start_time" not in st.session_state:
