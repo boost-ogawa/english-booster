8521c8befb75b1d203a185addbcb755985341cce
boost-ogawa <tak.ogawa.vc@gmail.com>
2025-05-29 14:53:36 +0900

    ページ３ラジオボタンフォントサイズ変更

--- app_j.py
+++ app_j.py
@@ -134,7 +134,7 @@ st.markdown(
     .japanese-translation {
         color: white;
         background-color: #333;
-        font-size: 1.2rem !important;
+        font-size: 1.5rem !important;
     }
 
     /* サブヘッダー (h2) のマージン調整 - これが最重要！ */
