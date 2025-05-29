65bc383a70ae8f341b0662e136ac3fab03df31a7
boost-ogawa <tak.ogawa.vc@gmail.com>
2025-05-29 13:19:43 +0900

    ページ３ラジオボタンフォントサイズ変更

--- app_j.py
+++ app_j.py
@@ -383,8 +383,6 @@ elif st.session_state.page == 4: # 結果表示ページ
             st.rerun()
 
     with col2: # 中央カラム: 英文
-        st.subheader("原文")
-        english_text = data.get('main', '原文がありません')
         st.markdown(
             f"""
             <div class="custom-paragraph">
@@ -394,8 +392,6 @@ elif st.session_state.page == 4: # 結果表示ページ
         )
 
     with col3: # 右カラム: 日本語訳
-        st.subheader("意味")
-        japanese_text = data.get('japanese', 'データがありません')
         st.markdown(
             f"""
             <div class="translation-text-block japanese-translation">
